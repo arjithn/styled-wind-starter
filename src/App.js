d418c245116f55b8e7e03a534f759c4679ea297d
Arjith Natarajan <arjith496@gmail.com>
2020-12-09 00:15:59 +0530

    Starter Page with Example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
+import styled from "styled-wind";
 import logo from "./style_wind_logo.svg";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>
-          Get the Best <br /> of Both Worlds <br /> in 1
-        </h1>
-        <img src={logo} alt="logo" />
+    <AppWrapper>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a target="_blank" rel="noopener noreferrer">
-          Docs
-        </a>
-        <a>GitHub</a>
-      </header>
-    </div>
+        <Row>
+          <Button
+            as="a"
+            href="https://styled-wind.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Docs
+          </Button>
+        </Row>
+      </AppHeader>
+    </AppWrapper>
   );
 }
+
+const Row = styled.div`
+.flex;
+.w-100;
+.flex-row;
+`;
+
+const Button = styled.button`
+  .bg-indigo-500;
+  .hover:bg-indigo-800;
+  .font-bold;
+  .text-lg;
+  .text-white;
+  .rounded-lg;
+  .w-32;
+  .p-2;
+  .cursor-pointer;
+`;
+
+const AppLogo = styled.img`
+  width: 40vmin;
+ .pointer-events-none;
+`;
+
+const AppHeader = styled.header`
+ .bg-teal-200;
+ .min-h-screen;
+ .flex;
+ .flex-col;
+ .items-center;
+ .justify-center;
+ font-size: calc(10px + 2vmin);
+ .text-orange-600;
+`;
+
+const AppWrapper = styled.div`
+ .text-center;
+ a{
+     color: auto;
+     .no-underline;
+ }
+`;
 
 export default App;
